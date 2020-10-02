0f8c5d9b18020259f29697da416531070ed6239d
@yzdshahin <shahin234yazdani@gmail.com>
2020-10-02 18:20:10 +0330

    new from entity and db connection

--- src/java/Model/Entity/LoginBean.java
+++ src/java/Model/Entity/LoginBean.java
@@ -6,51 +6,7 @@ public class LoginBean {
     
     private String username;
     private String password;
-    private String Fname;
-    private String Lname;
-    private String father;
-    private String birth;
-    private String gender;
-
-    public String getFname() {
-        return Fname;
-    }
-
-    public void setFname(String Fname) {
-        this.Fname = Fname;
-    }
-
-    public String getLname() {
-        return Lname;
-    }
-
-    public void setLname(String Lname) {
-        this.Lname = Lname;
-    }
-
-    public String getFather() {
-        return father;
-    }
-
-    public void setFather(String father) {
-        this.father = father;
-    }
-
-    public String getBirth() {
-        return birth;
-    }
-
-    public void setBirth(String birth) {
-        this.birth = birth;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+   
    
 
     public String getUsername() {
